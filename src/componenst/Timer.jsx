2bc12d9b906c43981f3d3fc93ebf0d7b56f0119b
DavidPopovich3261 <8534397@gmail.com>
2026-02-24 11:55:43 +0200

    timer

--- src/componenst/Timer.jsx
+++ src/componenst/Timer.jsx
@@ -1,30 +1,28 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Context } from '../Context';
-import { useInterval } from './useInterval'
+// import { useInterval } from './useInterval'
 
 function Timer() {
-    const { count, setCount } = useContext(Context())
-    const [timre, setTimre] = useState({
-        minutes: 2,
+    const { count, setCount } = useContext(Context)
+    const [timre, setTimre] = useState(70)
+    const [uitimer, setUitimer] = useState({
+        minutes: 0,
         seconds: 0
     })
     useEffect(() => {
         setTimeout(() => {
-            if (count != 0) {
+            setTimre(timre - 1)
+            if (timre < 0 && count != 0) {
                 return false
             }
-        }, );
-    }, [])
-
-    useEffect(() => {
-        useInterval(() => {
-            setTimre(timre - 1)
+            setUitimer({ minutes: (Math.floor(timre / 60)), seconds: (timre % 60) })
         }, 1000);
-    })
+    }, [timre])
 
     return (
         <div>
-
+            <p>{uitimer.minutes}:{uitimer.seconds}</p>
+            <p></p>
         </div>
     )
 }
