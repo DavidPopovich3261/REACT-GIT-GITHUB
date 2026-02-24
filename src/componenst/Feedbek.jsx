e6aa06976855406940a2e714e10b87985e3a6223
menahem_erlich <erlichmenahem@gmail.com>
2026-02-24 16:01:42 +0200

    משחקי css

--- src/componenst/Feedbek.jsx
+++ src/componenst/Feedbek.jsx
@@ -1,17 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Context } from '../Context'
 
 function Feedbek() {
     const {feedbek, setFeedbek, count, timer} = useContext(Context)
+    const [win, setWin] = useState(false)
     if (count === 0){
         setFeedbek("Winner!!!")
+        setWin(true)
     }
     if (timer < 0){
         setFeedbek("Game Over!")
     }
   return (
-    <div>
-      <h1>{feedbek}</h1>
+    <div className='feedbek-box'>
+      <h1 className={win? "win": "loss"}>{feedbek}</h1>
     </div>
   )
 }
